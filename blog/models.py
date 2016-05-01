078dd5ea8fa0d1f0afda754793d9a398f6f438e4
kmsohelinfo@gmail.com <kmsohelinfo@gmail.com>
2016-05-01 15:57:52 +0600

    Basic set up for blog

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length = 200)
+    text = models.TextField()
+
+
+    def __str__(self):
+        return self.title
