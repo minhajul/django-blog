f69758524c0da3dca9a828685994b6abe539330b
kmsohelinfo@gmail.com <kmsohelinfo@gmail.com>
2016-05-13 22:42:57 +0600

    Updated news section

--- news/views.py
+++ news/views.py
@@ -5,8 +5,8 @@ import urllib
 
 # Create your views here.
 def news_views(request):
-    news = {}
-    link = "http://en.prothom-alo.com/"
+    news = []
+    link = "http://prothom-alo.com/"
     page_link = urllib.urlopen(link)
     soup = BeautifulSoup(page_link, "html.parser")
     contents = soup.findAll('div', attrs = {'class': 'each_news'})
@@ -14,8 +14,6 @@ def news_views(request):
     for content in contents:
         title = content.findAll('h2', attrs = {'class': 'title'})[0].string
         news_summary = content.findAll('a', attrs = {'class': 'content_right'})[0].string
-        new_news = {'title': title, 'news_summary': news_summary}
-        news.update(new_news)
-
+        latest = {'title': title, 'news_summary': news_summary}
+        news.append(latest)
     return render(request, 'news.html', {'news': news})
-
